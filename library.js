6c2976004b3de3886d81c9c4b8b0df59dad44dca
Aodj05 <aodj05@msn.com>
2022-12-19 22:54:46 -0500

    functionality and layout

--- library.js
+++ library.js
@@ -1,31 +1,64 @@
+const bookCards = document.querySelector('.cards');
+const bookForm = document.querySelector('.form');
+const cards = document.querySelector('.book-cards');
+const showForm = document.querySelector('.new-book');
+showForm.addEventListener('click', () => {
+    cards.classList.add('active');
+})
+
+addNew.addEventListener('click', () => {
+    cards.classList.remove('active');
+})
+
 // Library array
 let myLibrary = [];
 
+
 // Object Constructor
-function Book(author, title, pages, read) {
-    this.author = author;
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
 }
+
+// Form to get user input and Submit user input form to the library
+function addBookForm() {
+    /* document.getElementById('.book-card').style.display = 'block'; */
+    bookForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        let book = new Book(title.value, author.value, pages.value, read.value);
+        myLibrary.push(book);
+        console.log(myLibrary);
+        console.log(book);
+        addBookToLibrary();
+        /* displayBook(book); */
+    });
+}
+addBookForm();
 
 // Add objects into the Library array
 function addBookToLibrary() {
-    bookCard.innerHTML = ''
-    for (var i = 0; i < bookCard.length; i++) {
+    bookCards.innerHTML = ''
+    for (var i = 0; i < myLibrary.length; i++) {
         displayBook(myLibrary[i]);
     }
 }
 
-// Form to get user input
-function bookForm() {
-    document.getElementById('bookCard').style.display = 'block';
+function displayBook(book) {
+    const bookCard = document.createElement('div');
+    bookCard.className = 'book-card';
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+    const bookAuthor = document.createElement('h4');
+    bookAuthor.textContent = `by ${book.author}`;
+    bookCard.appendChild(bookAuthor);
+    const bookPages = document.createElement('h5');
+    bookPages.textContent = `${book.pages} pages`;
+    bookCard.appendChild(bookPages);
+    bookCards.appendChild(bookCard);
 }
-
-// Submit user input form to the library
-document.getElementById('submit-form').addEventListener('click', function(e){
-    e.preventDefault()
-});
 
 // Deletes the book card from the library
 function deleteBook(title) {
